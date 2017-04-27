efc04f14fe7b08862a4cb7d243cce3b5c5ce77b3
Steve Mattis <steve.a.mattis@gmail.com>
2017-04-27 17:59:32 +0200

    adaptive sampling updates

--- bet/sampling/goalOrientedSampling.py
+++ bet/sampling/goalOrientedSampling.py
@@ -485,6 +485,7 @@ class sampler(bsam.sampler):
         #zs = np.greater(F_vec, 1.0e-12)
         #pdiff = self.disc_enhanced._input_sample_set._probabilities - self.disc._input_sample_set._probabilities
         cells = np.greater(np.fabs(self.disc_enhanced._input_sample_set._probabilities, self.disc._input_sample_set._probabilities), 1.0e-12)
+        cells_em = cells[self.disc._emulated_ii_ptr]
         #cells_m
         #self.disc._emulated_ii_ptr
         #cells = np.logical_and(cell, zs)
@@ -501,3 +502,320 @@ class sampler(bsam.sampler):
                                       
                 
             
+class sampler_hpl_adaptive(bsam.sampler):
+    """
+    This class provides methods for goal-oriented adaptive sampling of parameter 
+    space to provide samples to be used by algorithms to solve inverse problems. 
+    """
+    def __init__(self,
+                 lb_model_list,
+                 f,
+                 max_samples=int(1.0e6),
+                 min_samples=int(1.0e2),
+                 tol=1.0e-3,
+                 #error_estimates=False,
+                 jacobians=False):
+        
+        self.max_samples = max_samples
+        self.min_samples = min_samples
+        self.tol = tol
+        self.num_levels = len(lb_model_list)
+        self.lb_model_list = lb_model_list
+        self.f = f
+        self.error_estimates = True #error_estimates
+        self.jacobians = jacobians
+        self.total_samples = 0
+        self.total_evals = np.zeros((self.num_levels,), dtype=np.int)
+         
+
+    def initial_samples_random(self,
+                               sample_type,
+                               input_obj,
+                               num_samples=1E2,
+                               criterion='center',
+                               globalize=True,
+                               level=0,
+                               emulate=False,
+                               emulate_num=1E5,
+                               savefile='initial_discretization.txt.gz'):
+        initial_sampler = bsam.sampler(self.lb_model_list[level],
+                                       error_estimates = True, #self.error_estimates,
+                                       jacobians = self.jacobians)
+        input_obj = initial_sampler.random_sample_set(sample_type,
+                                          input_obj,
+                                          num_samples,
+                                          criterion,
+                                          globalize)
+        if emulate:
+            initial_sampler_em = bsam.sampler(self.lb_model_list[level],
+                                              error_estimates = True, #self.error_estimates,
+                                       jacobians = self.jacobians)
+            emulate_set = initial_sampler_em.random_sample_set(sample_type,
+                                                               input_obj,
+                                                               emulate_num,
+                                                               criterion,
+                                                               globalize)
+            
+        self.disc = initial_sampler.compute_QoI_and_create_discretization(
+            input_obj, savefile=savefile)
+        num = self.disc.check_nums()
+        self.disc._input_sample_set.set_levels(level*np.ones((num,), dtype=int))
+        self.total_samples += int(num)
+        self.total_evals[level] += int(num)
+        if emulate:
+            self.disc.set_emulated_input_sample_set(emulate_set)
+            self.fabs_em = np.fabs(self.f(self.disc._emulated_input_sample_set._values))
+        return self.disc
+
+    def initial_samples_regular(self,
+                                input_obj,
+                                level=0,
+                                num_samples_per_dim=1,
+                                savefile='initial_discretization.txt.gz'):
+    
+        initial_sampler = bsam.sampler(self.lb_model_list[level],
+                                       error_estimates = True, #self.error_estimates,
+                                       jacobians = self.jacobians)
+        input_obj = initial_sampler.regular_sample_set(input_obj,
+                                           num_samples_per_dim)
+        #import pdb
+        #pdb.set_trace()
+        self.disc = initial_sampler.compute_QoI_and_create_discretization(
+            input_obj, savefile=savefile)
+        num = self.disc.check_nums()
+        self.disc._input_sample_set.set_levels(level*np.ones((num,), dtype=int))
+        self.total_evals[level] += int(num)
+        return self.disc
+
+    def set_emulation_set(self,
+                          emulated_sample_set):
+        self.disc.set_emulated_input_sample_set(emulate_sample_set)
+        self.fabs_em = np.fabs(self.f(self.disc.emulated_input_sample_set._values))
+        #self.cells_nonzero = np.greater(self.fabs_em, 1.0e-12)
+    def set_probabilities(self,
+                          probabilities,
+                          probabilites_enhanced):
+        self.disc._input_sample_set.set_probabilities(probabilities)
+        self.probabilities_enhanced = probabilites_enhanced
+        self.prob_diff = np.fabs(probabilities - probabilites_enhanced)
+
+    def calculate_gamma(self):
+        self.disc.set_emulated_ii_ptr()
+        cells = np.greater(self.prob_diff, 1.0e-12)
+        cells_em = cells[self.disc._emulated_ii_ptr]
+        self.gamma = np.average(self.fabs_em[cells_em])        
+        return self.gamma
+
+    def calculate_ee_prob(self):
+        self.ee_prob = self.gamma*(self.prob_diff)
+        return self.ee_prob
+
+    def calculate_prob_ee_int_chain(self, x, x_enhanced):
+        num = self.disc.check_nums()
+        n1 = float(len(x))
+        n2 = float(len(x_enhanced))
+        F1 = self.f(x)
+        F2 = self.f(x_enhanced)
+        (_, ptr1) = self.disc._input_sample_set.query(x)
+        (_, ptr2) = self.disc._input_sample_set.query(x_enhanced)
+
+        int1 = np.zeros((num,))
+        int2 = np.zeros((num,))
+        prob1 = np.zeros((num,))
+        prob2 = np.zeros((num,))
+        
+        for i in range(num):
+            io1 = np.equal(ptr1, i)
+            int1[i] = np.sum(F1[io1])/n1
+            io2 = np.equal(ptr2, i)
+            int2[i] = np.sum(F2[io2])/n2
+            prob1[i] = float(np.sum(io1))/n1
+            prob2[i] = float(np.sum(io2))/n2
+            
+        self.ee_int = np.fabs(int1-int2)
+        self.disc._input_sample_set_probabilities = prob1
+        self.probabilities_enhanced = prob2
+        self.prob_diff = np.fabs(prob1 - prob2)
+        self.F1 = F1
+        self.F2 = F2
+        self.ptr1 = ptr1
+        self.ptr2 = ptr2
+        return self.ee_int, np.sum(int1), np.sum(int2)
+    
+    def calculate_ee_chain(self, x, x_enhanced):
+        #(_, ptr1) =  self.disc._input_sample_set.query(x)
+        #(_, ptr2) =  self.disc._input_sample_set.query(x)
+        #self.calculate_gamma()
+        #self.calculate_ee_prob()
+        (_, int1, int2) = self.calculate_prob_ee_int_chain(x, x_enhanced)
+        self.calculate_gamma()
+        self.calculate_ee_prob()
+        self.disc._input_sample_set._error_id = self.ee_int + self.ee_prob
+        return self.disc._input_sample_set._error_id, int1, int2
+
+    def calculate_subgrid_ee_chain(self, inds, subgrid_set, x, x_enhanced):
+        #num = self.disc.check_nums()
+        num_s = subgrid_set.check_num()
+        n1 = float(len(x))
+        n2 = float(len(x_enhanced))
+        #F1 = self.f(x)
+        #F2 = self.f(x_enhanced)
+        #(_, ptr1) = self.disc._input_sample_set.query(x)
+        #(_, ptr2) = self.disc._input_sample_set.query(x_enhanced)
+        #(_, ptr1) = subgrid_set.query(x)
+        #(_, ptr2) = subgrid_set.query(x_enhanced)
+
+        #int1 = np.zeros((num,))
+        #int2 = np.zeros((num,))
+        #prob1 = np.zeros((num,))
+        #prob2 = np.zeros((num,))
+        E_local = []
+        proposal_local = []
+        levels_local = []
+        
+        for j in inds:
+            x_loc1 = x[np.equal(self.ptr1, j),:]
+            x_loc2 = x[np.equal(self.ptr2, j),:]
+            (_, ptr1) = subgrid_set.query(x_loc1)
+            (_, ptr2) = subgrid_set.query(x_loc2)
+            #nums1 = len(x_loc1)
+            #nums2 = len(x_loc2)
+            #import pdb
+            #pdb.set_trace()
+            
+            int1 = np.zeros((num_s,))
+            int2 = np.zeros((num_s,))
+            prob1 = np.zeros((num_s,))
+            prob2 = np.zeros((num_s,))
+            for i in range(num_s):
+                io1 = np.equal(ptr1, i)#, np.equal(self.ptr1, j))
+                if np.sum(io1) > 0:
+                    int1[i] = np.sum(self.F1[io1])/n1
+                    prob1[i] = float(np.sum(io1))/n1
+                io2 = np.equal(ptr2, i)#, np.equal(self.ptr2, j))
+                if np.sum(io2) > 0:
+                    int2[i] = np.sum(self.F2[io2])/n2
+                    #prob1[i] = float(np.sum(io1))/n1
+                    prob2[i] = float(np.sum(io2))/n2
+            
+            ee_int = np.fabs(int1-int2)
+            #self.disc._input_sample_set_probabilities = prob1
+            #self.probabilities_enhanced = prob2
+            prob_diff = np.fabs(prob1 - prob2)
+            ee_prob = self.gamma * prob_diff 
+            ee = ee_int + ee_prob
+            E_local.append(np.sum(ee))
+            imax = np.argmax(ee)
+            proposal_local.append(subgrid_set._values[imax,:])
+            levels_local.append(self.disc._input_sample_set._levels[j])
+        E_local = np.array(E_local)
+        proposal_local = np.array(proposal_local)
+        return (E_local, proposal_local, levels_local)
+
+    def hl_step_chain(self, x, x_enhanced, subgrid_set, factor=0.2):
+        (ee, int1, int2) = self.calculate_ee_chain(x,x_enhanced)
+        max_ee = np.max(ee)
+        num_go = np.sum(np.greater(ee, factor*max_ee))
+        inds = np.argsort(ee)[::-1][0:num_go+1]
+        E = ee[inds]
+        (El, props, levels) = self.calculate_subgrid_ee_chain(inds, subgrid_set, x, x_enhanced)
+        hList=[]
+        propsList=[]
+        levelsList=[]
+        lList=[]
+        for i in range(len(inds)):
+            if El[i]/E[i] > 1.5:
+                hList.append(inds[i])
+                propsList.append(props[i])
+                levelsList.append(levels[i])
+            else:
+                lList.append(inds[i])
+
+        props = np.array(propsList)
+        levels = np.array(levelsList)
+        if len(hList) > 0:
+            self.h_refinement(props, levels)
+            self.disc._input_sample_set.kdtree = None
+        if len(lList) > 0:
+            self.level_refinement(lList)
+            
+        self.disc._io_ptr = None
+        self.disc._io_ptr_local = None            
+        
+        # import pdb
+        # pdb.set_trace()
+        
+    def level_refinement(self,
+                         indices):
+        """
+        Increases the level of selcted samples.
+
+        """
+        num_models = len(self.lb_model_list)
+        #good = np.less(self.disc._input_sample_set._levels, num_models - 1)
+
+        #indices = indices[good[indices]]
+        current_levels = self.disc._input_sample_set._levels[indices]
+        #import pdb
+        #pdb.set_trace()
+        for i in range(num_models-1, -1, -1):
+            #import pdb
+            #pdb.set_trace()
+            inds = indices[np.equal(current_levels, i)]
+            num_new = len(inds)
+            new_sset = sample.sample_set(self.disc._input_sample_set._dim)
+            new_sset.set_domain(self.disc._input_sample_set._domain)
+            new_sset.set_values(self.disc._input_sample_set._values[inds,:])
+
+   
+            new_sampler = bsam.sampler(self.lb_model_list[i],
+                                       error_estimates = self.error_estimates,
+                                       jacobians = self.jacobians)
+            new_disc = new_sampler.compute_QoI_and_create_discretization(
+                new_sset,
+                savefile=None,
+                globalize=True)
+            self.disc._output_sample_set._values[inds,:] = new_disc._output_sample_set._values[:]
+            self.disc._input_sample_set._levels[inds] = i
+            if self.error_estimates:
+                self.disc._output_sample_set._error_estimates[inds,:] = new_disc._output_sample_set._error_estimates[:]
+            if self.jacobians:
+                self.disc._input_sample_set._jacobians[inds,:] = new_disc._input_sample_set._jacobians[:]
+            self.total_evals[i] += num_new
+        #self.disc._io_ptr = None
+        #self.disc._io_ptr_local = None
+        #pdb.set_trace()
+
+        #def subgrid_ee_chain(
+        
+   
+    def h_refinement(self, props, levels):
+        for level in range(self.num_levels):
+            if level in levels:
+                inds = np.equal(levels, level)
+                new_vals = props[inds,:]
+                new_sset = sample.sample_set(self.disc._input_sample_set._dim)
+                new_sset.set_domain(self.disc._input_sample_set._domain)
+                new_sset.set_values(new_vals)        
+
+
+                num_new_samples = len(inds)
+                new_sampler = bsam.sampler(self.lb_model_list[level],
+                                               error_estimates = self.error_estimates,
+                                               jacobians = self.jacobians)
+                new_disc = new_sampler.compute_QoI_and_create_discretization(
+                    new_sset,
+                    savefile=None,
+                    globalize=True)
+                new_disc._input_sample_set.set_levels(level*np.ones((num_new_samples,), dtype=int))
+
+                self.disc._input_sample_set.append_sample_set(
+                    new_disc._input_sample_set)
+                self.disc._output_sample_set.append_sample_set(
+                    new_disc._output_sample_set)
+                self.total_evals[level] += num_new_samples
+                self.total_samples += num_new_samples
+        self.disc._input_sample_set.local_to_global()
+            
+
+        #return (prob, ee)
--- ex_gos/finite_diff/fd-refine/runBet2.py
+++ ex_gos/finite_diff/fd-refine/runBet2.py
@@ -15,89 +15,6 @@ alpha_2 = 1.0
 
 def F(lam):
     return np.greater(lam[:, 0], 1.00).astype(float)
-    #return np.sqrt(lam[:, 0]**2 + lam[:,1]**2)#return np.ones(lam[:,0].shape) #np.sqrt(lam[:, 0]**2 + lam[:,1]**2)
-
-def estimate_error(F1, p_meas1, p_meas2):
-    zero = np.equal(F1, 0.0)
-    not_zero = np.logical_not(zero)
-    e_id = np.zeros(F1.shape)
-    e_id[not_zero] =  alpha_1 * np.fabs(F1[not_zero])*np.fabs(p_meas1[not_zero] - p_meas2[not_zero])
-    zero_sum = alpha_2 * abs(np.sum(p_meas1[zero]) - np.sum(p_meas2[zero]))
-    e_id[zero] = zero_sum * np.fabs(p_meas1[zero] - p_meas2[zero])
-    return e_id
-
-def estimate_error2(disc, x1, x2, sset_fine=None):
-    F11 = F(x1)
-    F12 = F(x2)
-    #zero = np.logical_and(np.equal(F11, 0.0), np.equal(F12, 0.0))
-    zero1 = np.equal(F11, 0.0)
-    zero2 = np.equal(F12, 0.0)
-    num = disc.check_nums()
-    (_, id1) = disc._input_sample_set.query(x1)
-    (_, id2) = disc._input_sample_set.query(x2)
-    #import pdb
-    #pdb.set_trace()
-    e_id = np.zeros((num,))
-    #e_id2 = np.zeros((num,))
-    F1 = np.zeros((num,))
-    F2 = np.zeros((num,))
-    z1 = np.zeros((num,))
-    z2 = np.zeros((num,))
-
-    if sset_fine is not None:
-        num_fine = sset_fine.check_num()
-        F1_fine = np.zeros((num_fine,))
-        F2_fine = np.zeros((num_fine,))
-        z1_fine = np.zeros((num_fine,))
-        z2_fine = np.zeros((num_fine,))
-        (_, id1_fine) = sset_fine.query(x1)
-        (_, id2_fine) = sset_fine.query(x2)
-                        
-    #num_F1 = np.zeros((num,))
-    #num_F2 = np.zeros((num,))
-    for i in range(num):
-        ids1 = np.equal(id1,i)
-        if len(ids1) > 0:
-            F1[i] = np.sum(F11[ids1])/float(len(x1))
-            z1[i]= float(np.sum(zero1[ids1]))/float(len(x1))
-        ids2 = np.equal(id2,i)
-        if len(ids2) > 0:
-            F2[i] = np.sum(F12[ids2])/float(len(x2))
-            z2[i]= float(np.sum(zero2[ids2]))/float(len(x2))
-    # if sset_fine is not None:
-    #     for i in range(num_fine):
-    #         ids1_fine = np.equal(id1_fine,i)
-    #         if len(ids1_fine) > 0:
-    #             F1_fine[i] = np.sum(F11_fine[ids1])/float(len(x1))
-    #             z1[i]= float(np.sum(zero1[ids1]))/float(len(x1))
-    #         ids2 = np.equal(id2,i)
-    #         if len(ids2) > 0:
-    #             F2[i] = np.sum(F12[ids2])/float(len(x2))
-    #             z2[i]= float(np.sum(zero2[ids2]))/float(len(x2))
-        
-
-    e_id = alpha_1 * np.fabs(F2-F1) + alpha_2 * (abs(np.average(z2) - np.average(z1))* np.fabs(z2-z1))**0.5
-    # count1 = np.ones((len(x1),))
-    # count2 = np.ones((len(x2),))
-    # F1[id1] += F11
-    # F2[id2] += F12
-    # count_id1 = np.zeros((num,))
-    # count_id2 = np.zeros((num,))
-    # count_id1[id1] += count1
-    # count_id2[id2] += count2
-    # F1 = F1/count_id1
-    # F2 = F2/count_id2
-    # z1[id1[zero1]] += 1.0
-    # z2[id2[zero2]] += 1.0
-    # z1 = z1/count_id1
-    # z2 = z2/count_id2
-    # e_id = alpha_1 * np.fabs(F2-F1) + alpha_2 * (abs(np.average(z2) - np.average(z1))* np.fabs(z2-z1))**0.5
-  
-    return e_id
-        
-def mark(vals):
-    return np.less(vals[:,0], 1.0).astype(float)
-
 
 
 
@@ -107,11 +24,11 @@ int_exact = np.average(F(x_exact), axis=0)
 print 'Exact integral: ', int_exact
 
 # Run with surrogate
-s_set = samp.rectangle_sample_set(1)
-s_set.setup(maxes=[[3.0], [3.0]], mins=[[0.0], [-1.0]])
-s_set.set_region(np.array([0,1]))
+#s_set = samp.rectangle_sample_set(1)
+#s_set.setup(maxes=[[3.0], [3.0]], mins=[[0.0], [-1.0]])
+#s_set.set_region(np.array([0,1]))
 
-sampler = gos.sampler(1000, 10, 1.0e-5, [lb_model0, lb_model1, lb_model2, lb_model3, lb_model4], s_set, 0 , error_estimates=True, jacobians=True)
+sampler = gos.sampler_hpl_adaptive([lb_model0, lb_model1, lb_model2, lb_model3, lb_model4], f=F, jacobians=True)
 
 input_samples = samp.sample_set(2)
 domain = np.array([[0.0, 3.0], [-1.0, 3.0]])
@@ -120,8 +37,15 @@ input_samples.set_domain(domain)
 my_discretization = sampler.initial_samples_random('r',
                                                    input_samples,
                                                    100,
-                                                   level=0)
-for i in range(30):
+                                                   level=0,
+                                                   emulate=True,
+                                                   emulate_num=1E5)
+subgrid_set = bsam.random_sample_set('r', domain, num_samples=int(1E4))
+
+#import pdb
+#pdb.set_trace()
+
+for i in range(5):
     my_discretization._input_sample_set.set_kdtree()
 
     sur = surrogates.piecewise_polynomial_surrogate(my_discretization)
@@ -130,56 +54,74 @@ for i in range(30):
 
     (x2, y2, p_meas2) = run_mcmc(sur, N, order=1, ee=True)
     (x1, y1, p_meas1) = run_mcmc(sur, N, order=1, ee=False)
-    F1 = F(my_discretization._input_sample_set._values)
-
-    #(x2, y2, p_meas2) = run_mcmc(sur, N, order=1, ee=True)
-    #F2 = F(my_discretization._input_sample_set._values)
-    print "exact integral = ", int_exact
-    print 'P1 =  ', np.dot(F1, p_meas1), np.average(F(x1))
-    print 'P2 =  ', np.dot(F1, p_meas2), np.average(F(x2))
-
-    #e_id = estimate_error(F1, p_meas1, p_meas2)
-    e_id = estimate_error2(sampler.disc, x1, x2)
+    sampler.hl_step_chain(x1, x2, subgrid_set, 0.1)
+    #sampler.set_probabilities(p_meas1, p_meas2)
+    #F1 = F(my_discretization._input_sample_set._values)
+    #sampler.calculate_gamma()
+    #ee_prob = sampler.calculate_ee_prob()
+    #(ee, int1, int2) = sampler.calculate_ee_chain(x1,x2)
+    #inds = np.argsort(ee)[::-1][0:3]
+    #(E, p) = sampler.calculate_subgrid_ee_chain(inds, subgrid_set, x1, x2)
+    #sampler.hl_step_chain(x1, x2, subgrid_set, 0.1)
     #import pdb
     #pdb.set_trace()
-    marked = mark(my_discretization._input_sample_set._values)
+    #(x2, y2, p_meas2) = run_mcmc(sur, N, order=1, ee=True)
+    #F2 = F(my_discretization._input_sample_set._values)
+    # print "exact integral = ", int_exact
+    # print 'P1 =  ', int1 #np.dot(F1, p_meas1), np.average(F(x1))
+    # print 'P2 =  ', int2 #np.dot(F1, p_meas2), np.average(F(x2))
+    sampler.disc._io_ptr = None
+    sampler.disc._io_ptr_local = None
+
+    #e_id = estimate_error(F1, p_meas1, p_meas2)
+    #e_id = estimate_error2(sampler.disc, x1, x2)
+    #import pdb
+    #pdb.set_trace()
+    #marked = mark(my_discretization._input_sample_set._values)
+
+    e_id = sampler.disc._input_sample_set._error_estimates
     my_discretization._input_sample_set._probabilities_local = e_id
+    
     my_discretization._input_sample_set._probabilities = e_id
     
-    my_discretization._input_sample_set._error_id_local = e_id
-    my_discretization._input_sample_set._error_id = e_id
+    #my_discretization._input_sample_set._error_id_local = e_id
+    #my_discretization._input_sample_set._error_id = e_id
     #pv.plot_2D_voronoi(my_discretization, density=False, interactive=False)
     plotting.plot_hist(sampler.disc, x2, False, save=True, savename='hist_'+`i`)
     plotting.plot_error_id_voronoi(sampler.disc, False, save=True, savename='errorid_'+`i`)
     plotting.plot_levels_voronoi(sampler.disc, False, save=True, savename='levels_'+`i`, data_range=[1.0, 5.0])
 
-    if i%2 == 0:
-        # proposals
-        proposals = bsam.random_sample_set('random', input_obj=domain, num_samples=1000)
-        (_, cellList1) = proposals.query(x1)
-        (_, cellList2) = proposals.query(x2)
-        prop_meas1 = calc_measures(cellList1, proposals.check_num())
-        prop_meas2 = calc_measures(cellList2, proposals.check_num())
+    #(x2, y2, p_meas2) = run_mcmc(sur, N, order=1, ee=True)
+    #(x1, y1, p_meas1) = run_mcmc(sur, N, order=1, ee=False)
+    # sampler.hl_step_chain(x1, x2, subgrid_set, 0.1)
 
-        F1_p = F(proposals._values)
-        e_id_p = estimate_error(F1_p, prop_meas1, prop_meas2)
+    # if i%2 == 0:
+    #     # proposals
+    #     proposals = bsam.random_sample_set('random', input_obj=domain, num_samples=1000)
+    #     (_, cellList1) = proposals.query(x1)
+    #     (_, cellList2) = proposals.query(x2)
+    #     prop_meas1 = calc_measures(cellList1, proposals.check_num())
+    #     prop_meas2 = calc_measures(cellList2, proposals.check_num())
 
-        #proposals._probabilities_local = e_id_p
-        #proposals._probabilities = e_id_p
+    #     F1_p = F(proposals._values)
+    #     e_id_p = estimate_error(F1_p, prop_meas1, prop_meas2)
 
-        proposals._error_id = e_id_p
-        proposals._error_id_local = e_id_p
+    #     #proposals._probabilities_local = e_id_p
+    #     #proposals._probabilities = e_id_p
 
-        #sampler.h_refinement_cluster(proposals, order=0, num_new_samples=50, level=0, tol=0.005, match_level=True)
-        #print 'Total Samples after cluster ref. : ', sampler.disc.check_nums()
-        sampler.h_refinement_opt(proposals, order=0, num_new_samples=20, estimate_error=estimate_error2, x1=x2, x2=x2)
+    #     proposals._error_id = e_id_p
+    #     proposals._error_id_local = e_id_p
 
-    else:
-        sampler.level_refinement(None, 0, 20)
-        print 'Total Samples after level ref.: ', sampler.disc.check_nums()
-        for K in range(len(sampler.lb_model_list)):
-            Num = int(np.sum(np.equal(sampler.disc._input_sample_set._levels, K)))
-            print `Num` + " runs at level " + `K`
+    #     #sampler.h_refinement_cluster(proposals, order=0, num_new_samples=50, level=0, tol=0.005, match_level=True)
+    #     #print 'Total Samples after cluster ref. : ', sampler.disc.check_nums()
+    #     sampler.h_refinement_opt(proposals, order=0, num_new_samples=20, estimate_error=estimate_error2, x1=x2, x2=x2)
+
+    # else:
+    #     sampler.level_refinement(None, 0, 20)
+    #     print 'Total Samples after level ref.: ', sampler.disc.check_nums()
+    #     for K in range(len(sampler.lb_model_list)):
+    #         Num = int(np.sum(np.equal(sampler.disc._input_sample_set._levels, K)))
+    #         print `Num` + " runs at level " + `K`
     #plotting.plot_error_id_voronoi(sampler.disc, True)
 
 #proposals._probabilities_local = prop_meas2
